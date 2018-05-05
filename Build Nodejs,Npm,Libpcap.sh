527dc244d9e58a7c7d607791d5b824b6f4bd6143
LoveMyself666 <37284782+LoveMyself666@users.noreply.github.com>
2018-05-05 14:00:29 +0800

    Create Build Nodejs,Npm,Libpcap.sh

---
+++ Build Nodejs,Npm,Libpcap.sh
@@ -0,0 +1,16 @@
+echo "即将进行的是nodejs和npm的安装"
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
+source ~/.bash_profile
+nvm install v9.8.0
+nvm alias default v9.8.0
+echo "nodejs和npm安装完成，下面进行libpcap的安装"
+yum -y install gcc-c++
+yum -y install flex
+yum -y install bison
+wget http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+tar -zxvf libpcap-1.8.1.tar.gz
+cd libpcap-1.8.1
+./configure
+make
+make install
+echo "libpcap安装完成"
