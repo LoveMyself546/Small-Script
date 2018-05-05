c794a654d43956ede3f09f1ef034e42c3e538c07
LoveMyself666 <37284782+LoveMyself666@users.noreply.github.com>
2018-05-05 13:55:48 +0800

    Update Build SSR.sh

--- Build SSR.sh
+++ Build SSR.sh
@@ -1,5 +1,6 @@
+echo "即将进行ssr搭建"
+read -p "按下任意键继续"
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-
 chmod +x shadowsocks-all.sh
-
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+echo "ssr搭建完成，请记住/保存你的连接信息"
